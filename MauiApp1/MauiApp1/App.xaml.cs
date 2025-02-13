24ceaf8905300e9fbda11ff05e0b7a51c7fb877b
Anthony Ramos <ajramoslebron@gmail.com>
2025-02-13 15:13:38 -0600

    added tictactoe screen

--- MauiApp1/MauiApp1/App.xaml.cs
+++ MauiApp1/MauiApp1/App.xaml.cs
@@ -9,7 +9,7 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new MainPage()) { Title = "MauiApp1" };
+            return new Window(new MainPage()) { Title = "MauiApp1", Width = 1280, Height = 720 };
         }
     }
 }
